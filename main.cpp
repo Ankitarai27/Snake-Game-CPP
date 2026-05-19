1cd9f32740de343ca3c2bab28a057239c872d237
Ankita Rai <ankita.rai052005@gmail.com>
2026-05-19 11:57:48 +0530

    logic added

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ const int height=20;
 const int width=20;
 int headX,headY,fruitX,fruitY,score;
 int tail_len;
+int tailx[100],taily[100];
+
 
 void setup();
 void draw();
@@ -27,22 +29,180 @@ int main(){
     cin>>start;
     if(start=='s'){
         setup();
+        while(!gameOver){
+            draw();
+            input();
+            logic();
+            //make teh system slow
+            Sleep(30);
+            //remove earlier screen and we can 
+            //print next screen on same place
+            system("cls");
+
+        }
     }
     return 0;
 }
 
 void setup(){
-
+    gameOver=false;
+    dir=STOP;
+    headX=width/2;
+    headY=height/2;
+    fruitX=rand()%width;
+    fruitY=rand()%height;
+    score=0;
 }
 
 void draw(){
+    //so that older text will get remove
+    system("cls");
 
+    //upper border
+    cout<<"\t\t";
+    for(int i=0;i<width-8;i++){
+        cout<<"||";
+    }
+    cout<<endl;
+
+    //snake ,fruit , space and side border
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+
+            //left border
+            if(j==0){
+                cout<<"\t\t||";
+            }
+            
+            //snake head
+            if(i==headY && j==headX){
+                cout<<"O";
+            }
+
+            //fruit
+            else if(i==fruitY && j==fruitX){
+                cout<< "*";
+            }
+
+            //space , snake tail
+            else{
+                bool print=false;
+                //tail
+                for(int k=0;k<tail_len;k++){
+                    if(tailx[k]==j && taily[k]==i){
+                        cout<<"o";
+                        print=true;
+                    }
+                }
+                //space
+                if(!print){
+                    cout<<" ";
+                }
+            }
+
+            //right border
+            if(j==width-1){
+                cout<<"||";
+            }
+        }
+        cout<<endl;
+
+    }
+    
+
+     //lower border
+    cout<<"\t\t";
+    for(int i=0;i<width-8;i++){
+        cout<<"||";
+    }
+
+    cout<<endl;
+    cout<<"\t\t\t Score:" <<score<<endl;
 }
 
 void input(){
-
+    if(_kbhit())
+    switch(getch()){
+        case 'l':
+            dir=LEFT;
+            break;
+        case 'r':
+            dir=RIGHT;
+            break;
+        case 'u':
+            dir=UP;
+            break;
+        case 'd':
+            dir=DOWN;
+            break;
+        default:
+            break;
+    }
 }
 
 void logic(){
 
+    //tail logic
+    int prevx=tailx[0];
+    int prevy=taily[0];
+    int prev2x, prev2y;
+    tailx[0]=headX;
+    taily[0]=headY;
+    for(int i=1;i<tail_len;i++){
+        prev2x=tailx[i];
+        prev2y=taily[i];
+        tailx[i]=prevx;
+        taily[i]=prevy;
+        prevx=prev2x;
+        prevy=prev2y;
+    }
+
+    //direction logic
+    switch(dir){
+        case LEFT:
+            headX--;
+            break;
+        case RIGHT:
+            headX++;
+            break;
+        case UP:
+            headY--;
+            break;
+        case DOWN:
+            headY++;
+            break; 
+        default:
+            break;
+    }
+
+    //touch walls
+    if(headX>=width){
+        headX=0;
+    }
+    else if(headX<0){
+        headX=width-1;
+    }
+    if(headY>=height){
+        headY=0;
+    }
+    else if(headY<0){
+        headY=height-1;
+    }
+
+
+    //snake bit itself
+    for(int i=0;i<tail_len;i++){
+        if(tailx[i]==headX && taily[i]==headY){
+            gameOver=true;
+        }
+    }
+
+
+    //snake eat fruit
+    if(headX==fruitX && headY==fruitY){
+        score+=10;
+        fruitX=rand()%width;
+        fruitY=rand()%height;
+        tail_len+=1;
+    }
 } 
