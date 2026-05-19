d909fb6e584d48ca8710915f2493813c6b528e4e
Ankita Rai <ankita.rai052005@gmail.com>
2026-05-19 10:12:50 +0530

    added main

---
+++ README.md
@@ -0,0 +1,3 @@
+# Snake-Game-CPP
+
+Learning C++ by building a Snake Game while following a tutorial.
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+#include<Windows.h>
+using namespace std;
+int main(){
+    char start;
+    cout<<"\t-----------------------"<<endl;
+    cout<<"\t\t :Snake King:"<<endl;
+    cout<<"\t------------------------"<<endl;
+    cout<<"Press 's' to start: ";
+    cin>>start;
+    if(start=='s'){
+        cout<< "\tGame Start";
+    }
+    return 0;
+}
